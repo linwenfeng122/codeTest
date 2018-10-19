1e655e2d543567111dd3d077a9a8aa5539ae53c6
lwf <812174597@qq.com>
2018-10-19 16:02:59 +0800

    基础的文件

---
+++ Vue/Link/component.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+       <app-link to="1" >a链接</app-link>
+       <app-link to="2" >router-link</app-link>
+    </div>
+</template>
+
+<script>
+import appLink from './link'
+export default {
+
+  components: {
+    appLink
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ Vue/Link/link.vue
@@ -0,0 +1,35 @@
+<template>
+	<component v-bind="linkProps(to)">
+    <slot/>
+  </component>
+</template>
+
+<script>
+export default {
+ props: {
+    to: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    linkProps(to) {
+       if (to == "1") {
+       	  return {
+       	  	 is: 'a',
+       	  	 href: 'http://www.baidu.com'
+       	  }
+       }else{
+       	  return {
+       	  	is: 'router-link',
+       	  	to: '/test'
+       	  }
+       }
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
